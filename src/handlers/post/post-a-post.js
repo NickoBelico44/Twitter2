58f25915f9db91b8acbb30c98054c682770442a2
Jesús Alberto López Corona <locjmf@gmail.com>
2022-06-09 22:55:33 -0500

    post shows

--- src/handlers/post/post-a-post.js
+++ src/handlers/post/post-a-post.js
@@ -1,18 +1,28 @@
 const { getConnection } = require("../../utils/db.utils")
 
-module.exports = async (req, res) => {
-  const { title, content } = req.body
-  const email = req.header('X-Email')
-  const connection = await getConnection()
-  const [result] = await connection.execute('INSERT INTO POST_POST (creator_email,title,content) VALUES (?, ?, ?)', [email, title, content])
-  const { insertId: id} = result 
-  console.log(' - result:', result)
-  const [result2] = await connection.execute('SELECT * FROM POST_POST JOIN APPUSER_POST ON APPUSER_POST.email = POST_POST.creator_email WHERE POST_POST.id = ?', [id])  
-  console.log(result2);
-  const { created_at: created_at, name: name} = result2 
-  const author = {
-    email: email,
-    name: name
-  }
-  res.json({ id, title, content, created_at, author})
+module.exports = async(req, res) => {
+    const { title, content } = req.body
+    const email = req.header('X-Email')
+    const connection = await getConnection()
+    const [result] = await connection.execute('INSERT INTO POST (creator_email,title,content) VALUES (?, ?, ?)', [email, title, content])
+    const { insertId: id } = result
+    let response = { error: 'ERROR' }
+    const [rows] = await connection.query('SELECT * FROM POST JOIN APPUSER ON APPUSER.email = POST.creator_email WHERE POST.id = ?', [id])
+    const [row] = rows
+    console.log(row)
+    if (!rows.length) {
+        res.status(404).end()
+        return
+    } else {
+        const {
+            id,
+            title,
+            content,
+            created_at,
+            name,
+            email
+        } = row
+        response = { id, title, content, created_at, author: { email, name } }
+    }
+    res.json(response)
 }
